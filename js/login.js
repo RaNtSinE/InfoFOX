eab4f1754e6157b0bd368a94af372973157f0237
RaNtSinE <45504704+RaNtSinE@users.noreply.github.com>
2020-06-24 13:58:05 +0700

    minus interval capthca

--- js/login.js
+++ js/login.js
@@ -1,28 +1,3 @@
-setInterval(function () {
-    let dataReCaptcha = $("#g-recaptcha-response").val();
-    capt = document.getElementsByClassName("g-recaptcha");
-    ent = document.getElementsByClassName("enter");
-    if (dataReCaptcha != "" && dataReCaptcha != undefined) {
-        setTimeout(function () {
-            capt[0].classList.add("almostHide");
-            ent[0].classList.add("enter-visible");
-            setTimeout(function () {
-                capt[0].classList.add("hide");
-            }, 500)
-        }, 100);
-    }
-    else
-    {
-        setTimeout(function () {
-            capt[0].classList.remove("hide");
-            setTimeout(function () {
-                capt[0].classList.remove("almostHide");
-                ent[0].classList.remove("enter-visible");
-            }, 10)
-        }, 100);
-    }
-}, 1000);
-
 $('.enter').on('click',function()
 {
     let username = document.getElementById('id_username');
